ffc03293cf624d052117aa07721035770000bc0f
Shane Blatt <shane.blatt@yahoo.com>
2015-01-27 17:16:11 -0500

    template update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,6 @@
 // });
 
 // var shane = user, orgs, repo, starred;
-// 	console.log(user.url);
 // 	console.log(user.name);
 // 	console.log(user.login);
 // 	console.log(user.location);
@@ -15,18 +14,78 @@
 // 	console.log(user.following);
 
 
+var user = "https://api.github.com/users/Shane40";
+var orgs = "https://api.github.com/orgs/tiy-atl-js-q1-2015";
+var repo = "https://api.github.com/users/Shane40/repos";
+var starred = "https://api.github.com/users/h5bp";
+
 
 // =====USER======
 
-// Grab the template string.
-var templateString = $('#item-template').text();
+// Grab the template.
+var usertemplate = $('#user-template').html();
 
 // Turn it into a template function.
-var renderTemplate = _.template(templateString);
+var userrendered = _.template(usertemplate);
 
 // Pass in an object. Return value is a string
 // with the bee stings replaced with object's properties
-var freshHTML = renderTemplate({name: 'Beer Mug'});
+ $.getJSON(user).done (function (i){
+ $('.some-user').append(userrendered(i));
+ });
 
 // Inject the fresh html into the page.
-$('.some-container').append(freshHTML);
+// $('.some-container').append(freshHTML);
+
+
+// =======END OF USER=========
+
+// ======REPO =======
+
+// Grab the template.
+var repotemplate = $('#repo-template').html();
+
+// Turn it into a template function.
+var reporendered = _.template(repotemplate);
+
+// Pass in an object. Return value is a string
+// with the bee stings replaced with object's properties
+ $.getJSON(repo).done (function (i){
+ 	console.log(i);
+ 	i.forEach(function (x){
+ $('.some-repo').append(reporendered(x));
+	});
+ });
+
+
+
+// ======END OF REPO =======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
