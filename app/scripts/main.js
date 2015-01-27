c58111da57591d3397f708923cd0f502bfa50585
Shane Blatt <shane.blatt@yahoo.com>
2015-01-26 21:18:17 -0500

    tabs working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,8 +8,54 @@ console.log('The Iron Yard Rocks');
 // 	console.log(listing.Images[0].url_fullxfull);
 // 	console.log(listing.Shop.shop_name);
 
+// productboxHTML +=
+// '<div class="productbox">' +
+// 	'<img src= "'+listing.Images[0].url_fullxfull+'"/> ' +
+// 	'<h3>'+ listing.title+' </h3>' + 
+// 	'<span> <h4>'+ listing.Shop.shop_name+' </h4> </span>' +
+// 	'<span> <h5>'+ listing.price+' '+ listing.currency_code+'</h5> </span>' +
+// '</div>'
+
+// console.log (productboxHTML);
+
+// //give the function a name that represents a concept.
+// });
+
+
+// return productboxHTML;
+// };
+
+// $(document).ready(function(){
+// 	$(".rightbox").append(getproduct());
+
+// });
+//=======USER======
+
+	// PHOTO
+	// console.log(user.avatar_url);
+
+	// console.log(user.name);
+
+
+var shanemug = $('.hero-unit img');
+console.log(shanemug);
+
+
+
+// var templateFunct = _.template(templateString)
+
+// $(".leftbox").append(xxxxx());
+
+// });
+
+
+// =====REPO======
 repo.forEach (function (identity){
 	console.log(identity.name);
 });
 
+repo.forEach(function (weblist){
+	console.log(weblist.url);
+	console.log(weblist.owner.url)
 
+});
