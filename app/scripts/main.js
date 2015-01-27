713965c3d84a595669a296c12157373a806de00b
Shane Blatt <shane.blatt@yahoo.com>
2015-01-26 22:14:31 -0500

    more JS work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,3 @@
-console.log('The Iron Yard Rocks');
-
 // function getproduct() {
 // var productboxHTML = "";
 // etsy_items.results.forEach ( function (listing){
@@ -31,14 +29,46 @@ console.log('The Iron Yard Rocks');
 // });
 //=======USER======
 
-	// PHOTO
-	// console.log(user.avatar_url);
-
-	// console.log(user.name);
+// =====USER======
 
 
-var shanemug = $('.hero-unit img');
-console.log(shanemug);
+function mainbiobox (){
+var biobox = 
+	console.log(user.url);
+	console.log(user.name);
+	console.log(user.login);
+	console.log(user.location);
+	console.log(user.email);
+	console.log("Joined on " + user.created_at);
+	console.log(user.followers);
+	console.log(user.public_gists);
+	console.log(user.following);
+
+	biobox +=
+	'<div class="shanebox">' +
+	'<img scr="'+user.url+'"/> ' +
+	'<li>' + user.name+' </li>' +
+	'<li>' + user.login+' </li>' +
+	'<li>' + user.location+' </li>' +
+	'<li>' + user.email+' </li>' +
+	'<li>' + user.created_at+' </li>' +
+	'<h4>' + user.followers+' </h4>' +
+	'<h4>' + user.public_gists+' </h4>' +
+	'<h4>' + user.following+' </h4>' +
+	'</div>'
+});
+	console.log(mainbiobox);
+
+
+// '<div class="productbox">' +
+// 	'<img src= "'+listing.Images[0].url_fullxfull+'"/> ' +
+// 	'<h3>'+ listing.title+' </h3>' + 
+// 	'<span> <h4>'+ listing.Shop.shop_name+' </h4> </span>' +
+// 	'<span> <h5>'+ listing.price+' '+ listing.currency_code+'</h5> </span>' +
+// '</div>'
+
+// var shanemug = $('.hero-unit img');
+// console.log(shanemug);
 
 
 
@@ -49,13 +79,17 @@ console.log(shanemug);
 // });
 
 
+
+
+
+
 // =====REPO======
-repo.forEach (function (identity){
-	console.log(identity.name);
-});
+// repo.forEach (function (identity){
+// 	console.log(identity.name);
+// });
 
-repo.forEach(function (weblist){
-	console.log(weblist.url);
-	console.log(weblist.owner.url)
+// repo.forEach(function (weblist){
+// 	console.log(weblist.url);
+// 	console.log(weblist.owner.url)
 
-});
+// });
